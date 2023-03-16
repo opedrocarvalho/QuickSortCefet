56259a80833fd6766a49747d63c950c61bb3d0b9
Pedro Carvalho <113921418+opedrocarvalho@users.noreply.github.com>
2023-03-16 13:35:08 -0300

    Add files via upload

---
+++ Quick Sort.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void Quicksort(int vet[], int l, int r) {
+    int pivot = vet[l + (r - l) / 2];
+    /*
+	Atribui-se ao pivot o índice do meio do array, para que
+	seja mais rápido
+	
+	Assim, pivot = vet[0 + (8 - 0)/2] -> vet[4] = pivot
+	*/
+    int left = l, right = r;
+    
+    while (left <= right) {
+        while (vet[left] < pivot) left++;
+        while (vet[right] > pivot) right--;
+        if (left <= right) {
+            int temp = vet[left];
+            vet[left] = vet[right];
+            vet[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    
+    if (l < right) Quicksort(vet, l, right);
+    if (left < r) Quicksort(vet, left, r);
+}
+
+int main() {
+    int v[] = {6, 1, 8, 3, 9, 2, 10, 7};
+    int length = sizeof(v) / sizeof(int);
+    
+    Quicksort(v, 0, length - 1);
+    
+    for (int i = 0; i < length; i++)
+        printf("%d ", v[i]);
+    printf("\n");
+    
+    return 0;
+}
