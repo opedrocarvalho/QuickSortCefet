2202ad8b419b8d9fa751fc126d7ae37fdfd528c8
Manoel-Mieiro <102331096+Manoel-Mieiro@users.noreply.github.com>
2023-03-22 12:01:56 -0300

    Add files via upload
    Adicionada a função de quicksort. Ainda não concluído.

--- gabarito/3.cpp
+++ gabarito/3.cpp
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct cartas card;
  
@@ -7,4 +9,35 @@ typedef struct cartas card;
     char cardgame_name[50];   // Nome do jogo de cartas
     char raridade[20];        // Raridade da carta (comum, raro, super raro, ex, full art)
 };
+
+void quicksort(card vetor[], int inicio, int fim) {
+    int i, j;
+    struct card pivo, temp;
+    i = inicio;
+    j = fim - 1;
+    pivot = vetor[(inicio + fim) / 2];
+
+    while (i <= j) {
+        while (strcmp(vetor[i].raridade, pivot.raridade) > 0) {
+            i++;
+        }
+        while (strcmp(vetor[j].raridade, pivot.raridade) < 0) {
+            j--;
+        }
+        if (i <= j) {
+            temp = vetor[i];
+            vetor[i] = vetor[j];
+            vetor[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    if (inicio < j) {
+        quicksort(vetor, inicio, j+1);
+    }
+    if (i < fim) {
+        quicksort(vetor, i, fim);
+    }
+}
 
