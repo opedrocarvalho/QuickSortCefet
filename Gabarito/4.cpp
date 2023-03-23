dc37d4893ee29f3956c00088c2f56f735eca4c55
Manoel-Mieiro <102331096+Manoel-Mieiro@users.noreply.github.com>
2023-03-23 11:44:49 -0300

    Add files via upload

--- Gabarito/4.cpp
+++ Gabarito/4.cpp
@@ -2,81 +2,91 @@
 #include <stdlib.h>
 
 typedef struct no No;
- struct no{
-  int dado;
-  No *ant;
-  No *prox;
+struct no
+{
+    int dado;
+    No *ant;
+    No *prox;
 } *inicio = NULL, *fim = NULL;
 
-void quicksort(No *inicio, No *fim) {
-  if (fim != NULL && inicio != fim && inicio != fim->prox) {
-    int pivo = fim->dado;
-    No *i = inicio->ant;
+void quicksort(No *inicio, No *fim)
+{
+    if (fim != NULL && inicio != fim && inicio != fim->prox)
+    {
+        int pivo = fim->dado;
+        No *i = inicio->ant;
 
-    for (No *j = inicio; j != fim; j = j->prox) {
-      if (j->dado < pivo) {
-        i = (i == NULL) ? inicio : i->prox;
+        for (No *j = inicio; j != fim; j = j->prox)
+        {
+            if (j->dado < pivo)
+            {
+                i = (i == NULL) ? inicio : i->prox;
+                int temp = i->dado;
+                i->dado = j->dado;
+                j->dado = temp;
+            }
+        }
+
+        if (i == NULL)
+        {
+            i = inicio;
+        }
+        else
+        {
+            i = i->prox;
+        }
+
         int temp = i->dado;
-        i->dado = j->dado;
-        j->dado = temp;
-      }
-    }
+        i->dado = fim->dado;
+        fim->dado = temp;
 
-	if (i == NULL){
-    	i = inicio;
+        quicksort(inicio, i->ant);
+        quicksort(i->prox, fim);
     }
-    else{
-   	 	i = i -> prox;	
-	}
-	
-    int temp = i->dado;
-    i->dado = fim->dado;
-    fim->dado = temp;
-
-    quicksort(inicio, i->ant);
-    quicksort(i->prox, fim);
-  }
 }
 
-
-void imprimirLista() {
-	No *aux = inicio;
-  while (aux != NULL) {
-    printf("%d ", aux->dado);
-    aux = aux->prox;
-  }
-  printf("\n");
+void imprimirLista()
+{
+    No *aux = inicio;
+    while (aux != NULL)
+    {
+        printf("%d ", aux->dado);
+        aux = aux->prox;
+    }
+    printf("\n");
 }
 
-int main() {
-  int n, x;	
+int main()
+{
+    int n, x;
 
-  printf("Digite o tamanho da lista: ");
-  scanf("%d", &n);
+    printf("Digite o tamanho da lista: ");
+    scanf("%d", &n);
 
-  printf("Digite os elementos da lista:\n");
+    printf("Digite os elementos da lista:\n");
 
-  for (int i = 0; i < n; i++) {
-    scanf("%d", &x);
-    No *novo_no = new No();
-    novo_no->dado = x;
-    novo_no->ant = fim;
-    novo_no->prox = NULL;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
+        No *novo_no = new No();
+        novo_no->dado = x;
+        novo_no->ant = fim;
+        novo_no->prox = NULL;
 
-    if (fim != NULL)
-      fim->prox = novo_no;
-    fim = novo_no;
-    if (inicio == NULL)
-      inicio = fim;
-  }
+        if (fim != NULL)
+            fim->prox = novo_no;
+        fim = novo_no;
+        if (inicio == NULL)
+            inicio = fim;
+    }
 
-  printf("Lista original: ");
-  imprimirLista();
+    printf("Lista original: ");
+    imprimirLista();
 
-  quicksort(inicio, fim);
+    quicksort(inicio, fim);
 
-  printf("Lista ordenada: ");
-  imprimirLista();
+    printf("Lista ordenada: ");
+    imprimirLista();
 
-  return 0;
+    return 0;
 }
